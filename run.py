07ce0ea4b0bb74cb16559f202624a2cc50989a4d
Elisabeth Oelmann <elisabethoelmann@icloud.com>
2024-05-16 12:46:27 +0000

    test example data in KaplanMeier curve and display plot

--- run.py
+++ run.py
@@ -5,6 +5,27 @@ import matplotlib.pyplot as plt
 
 # Your code goes here.
 
+# Create my data frame
+data = pd.DataFrame({
+    'Time': [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+    'Event': [1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+    'Group': ['Group A', 'Group A', 'Group A', 'Group A', 'Group B', 'Group B', 'Group B', 'Group B', 'Group B', 'Group B', 'Group B']
+   })
+
+# Create the KaplanMeier object and fit the data
+
+kmf = KaplanMeierFitter()
+kmf.fit(data['Time'], data['Event'], label='Overall Survival')
+
+# plot the KaplanMeier curve
+
+fig, ax = plt.subplots()
+kmf.plot(ax=ax)
+ax.set_xlabel('Time')
+ax.set_ylabel('Survival Probability')
+ax.set_title('Kaplan-Meier Curve')
+plt.savefig('km_plot.png')
+
 
 
 
