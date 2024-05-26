fd77d9ea7607eb62731d90d5227437a3602250c8
Elisabeth Oelmann <elisabethoelmann@icloud.com>
2024-05-26 18:35:03 +0000

    attempt to fix the new user data up-load to Google sheet

--- run.py
+++ run.py
@@ -96,102 +96,89 @@ print("Median PFS for Group A:{:.2f}".format(median_pfs_group_A))
 print("Median PFS for Group B:{:.2f}".format(median_pfs_group_B))
 print("Median PFS for Group C:{:.2f}".format(median_pfs_group_C))
 
-# 2nd part of the code to enable the user to enter new data and feed the new data back into the Google sheet
+# 2nd part of the code to enable the user to enter new data, re-plot the Kaplan-Meier curve with new data and feed the new data back into the Google sheet
 
 # Prompt user for new data input
 # Validation of user input (valid data for "time" 0-15 with max.1 decimal place and "event" valid data either 0 or 1)
-new_data = input("Please enter new survival data")
 data_str = input("Enter your data here:\n")
-new_data_a = []
-new_data_b = []
-new_data_c = []
+new_data_a = [(None, None)] * 13
+new_data_b = [(None, None)] * 13
+new_data_c = [(None, None)] * 13
 
 for i in range(13):
     while True:
         time_a = input("Enter Time for Group A:")
-        if 0 <=float(time_a) <= 15 and time_a.count('.') <= 1:
+        if 0 <= float(time_a) <= 15 and time_a.count('.') <= 1:
             break
         else:
             print("Invalid input. Please enter a number between 0-15 with 1 decimal place")
 
     while True:
         event_a = input("Enter Event for Group A:")
-        if event_a.isdigit() and int(event_a) in[0,1]:
+        if event_a.isdigit() and int(event_a) in [0,1]:
+            new_data_a[i] = (time_a, event_a)
             break
         else:
             print("invalid input. Please enter either 0 or 1.")
 
-    if i < len(new_data_a):
-        new_data_a[i] = (time_a,event_a)
-    else:
-        new_data_a.append((time_a, event_a))
-
     while True:
         time_b = input("Enter Time for Group B:")
-        if 0 <=float(time_b) <=15 and time_b.count('.') <= 1:
+        if 0 <= float(time_b) <= 15 and time_b.count('.') <= 1:
             break
         else:
             print("Invalid input. Please enter a number between 0-15 with 1 decimal place")
     while True:
         event_b = input("Enter Event for Group B:")
-        if event_b.isdigit() and int(event_b) in[0,1]:
+        if event_b.isdigit() and int(event_b) in [0,1]:
+            new_data_b[i] = (time_b, event_b)
             break
         else:
             print("Invalid input. Please enter either 0 or 1")
 
-    if i < len(new_data_b):
-        new_data_b[i] = (time_b,event_b)
-    else:
-        new_data_b.append((time_b, event_b))
-
-
+       
     while True:
         time_c = input("Enter Time for Group C:")
-        if 0 <=float(time_c) <= 15 and time_c.count('.') <= 1:
+        if 0 <= float(time_c) <= 15 and time_c.count('.') <= 1:
             break
         else:
             print("Invalid input. Please enter a number between 0-15 with 1 decimal place.")
 
     while True:
         event_c = input("Enter Event for Group C:")
-        if event_c.isdigit() and int(event_c) in[0,1]:
+        if event_c.isdigit() and int(event_c) in [0,1]:
+            new_data_c[i] = (time_c, event_c)
             break
         else:
             print("Invalid input. Please enter either 0 or 1.")
 
-    if i < len(new_data_c):
-        new_data_c[i] = (time_c,event_c)
-    else:
-        new_data_c.append((time_c, event_c))
-
 
 # Replace existing data with new user input for all rows:
-for i in range(13):
-    #add the check for index range
-    if i<len(new_data_a):
-        data.loc[(data['Group'] == 'A') & (data.index == i),['Time','Event']] = [float(new_data_a[i][0]),bool(new_data_a[i][0])]
+# Update the data in the appropriate index range
+for i, (data_a, data_b, data_c) in enumerate(zip(new_data_a, new_data_b, new_data_c)):
+    start_index_a = 2 + i
+    end_index_a = 14 + i
+    start_index_b = 15 + i
+    end_index_b = 27 + i
+    start_index_c = 28 + i
+    end_index_c = 40 + i
+    
+    if end_index_a <= len(data):
+        data.loc[(data['Group'] == 'A') & (data.index >= start_index_a) & (data.index <= end_index_a), ['Time', 'Event']] = [float(data_a[0]), int(data_a[1])]
     else:
-    # handle the case when out of range
-        print("Index out of range")
+        print("Index out of range for Group A")
+    
+    if end_index_b <= len(data):
+        data.loc[(data['Group'] == 'B') & (data.index >= start_index_b) & (data.index <= end_index_b), ['Time', 'Event']] = [float(data_b[0]), int(data_b[1])]
+    else:
+        print("Index out of range for Group B")
 
-for i in range(13):
-    #add the check for index range
-    if i<len(new_data_b):
-        data.loc[(data['Group'] == 'B') & (data.index == i),['Time','Event']] = [float(new_data_b[i][0]),bool(new_data_b[i][0])]
+    if end_index_c <= len(data):
+        data.loc[(data['Group'] == 'C') & (data.index >= start_index_c) & (data.index <= end_index_c), ['Time', 'Event']] = [float(data_c[0]), int(data_c[1])]
     else:
-    # handle the case when out of range
-        print("Index out of range")
-
-for i in range(13):
-     #add the check for index range
-    if i<len(new_data_c):
-        data.loc[(data['Group'] == 'C') & (data.index == i),['Time','Event']] = [float(new_data_c[i][0]),bool(new_data_c[i][0])]
-    else:
-    # handle the case when out of range
-        print("Index out of range")
+        print("Index out of range for Group C")
 
 
 # up-date the Google Excel sheet with the modified data
 worksheet = SHEET.worksheet('ABC')
-worksheet.update([data.columns.values.tolist()]+data.values.tolist())
+worksheet.update([data.columns.values.tolist()] + data.values.tolist())
 
